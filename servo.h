6fef187b15e43c8d0b0017caa6bd7a38bde78c99
hirobon1690 <hirobon1690@gmail.com>
2023-06-08 16:49:33 +0900

    add servo

---
+++ servo.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "stdio.h"
+
+class Servo {
+   public:
+    Servo(int pin);
+    void init();
+    void write(float val);
+    float read();
+    void write_ms(float ms);
+
+   private:
+    int pin;
+    uint slice_num;
+    uint channel;
+};
